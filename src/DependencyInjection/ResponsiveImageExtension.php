c9f72aa86018c4a41d9641ba14d2be8415fcf1e5
Aleksey Razbakov <aleksey.razbakov@ommax.de>
2024-12-14 01:02:50 +0100

    add services

--- src/DependencyInjection/ResponsiveImageExtension.php
+++ src/DependencyInjection/ResponsiveImageExtension.php
@@ -6,14 +6,14 @@ use Ommax\ResponsiveImageBundle\Provider\ProviderInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ResponsiveImageExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
