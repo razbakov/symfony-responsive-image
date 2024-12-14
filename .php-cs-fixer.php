5bb4a8f21a2b0eb31a07d1181bcfb5e946014508
Aleksey Razbakov <aleksey.razbakov@ommax.de>
2024-12-14 03:11:45 +0100

    fix php cs

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,13 @@ use PhpCsFixer\Finder;
 return (new Config())
     ->setUsingCache(true)
     ->setRiskyAllowed(true)
-    ->setFinder((new Finder())->in(__DIR__))
+    ->setFinder(
+        (new Finder())
+            ->in([
+                __DIR__.'/src',
+                __DIR__.'/tests',
+            ])
+    )
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
